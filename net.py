62ef88397f2963adf76bb6095d3d1d69a0e5ff20
Bradley Davis <bad884@me.com>
2018-03-20 16:03:37 -0500

    Add node and node weight pair

--- net.py
+++ net.py
@@ -1,10 +1,17 @@
 from scipy.io import arff
+from enum import Enum
 
 import collections
 import math
 import random
 import sys
 
+
+DEBUG = False
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
 def create_instances_from_arff_data(train_data, attr_names, attr_types, attr_values):
     instances = []
@@ -30,42 +37,41 @@ def standardize_instances(instances, attr_names, attr_types, attr_values):
         if _type == 'numeric':
             attr_sums[attr_name] = 0.0
             attr_sum_sq_diffs[attr_name] = 0.0
-
     # Sum up values
     for instance in instances:
         for attr_value in instance.attributes:
             if attr_names[instance.attributes.index(attr_value)] in attr_sums.keys():
                 attr_sums[attr_names[instance.attributes.index(attr_value)]] += attr_value
-    # print()
-    # for attr_name, _sum in attr_sums.items():
-    #     print('name: {}\tsum = {}'.format(attr_name, _sum))
-
+    if DEBUG:
+	print()
+	for attr_name, _sum in attr_sums.items():
+	    print('name: {}\tsum = {}'.format(attr_name, _sum))
     # Calculate means
     attr_means = collections.OrderedDict()
     for attr_name, _sum in attr_sums.items():
         attr_means[attr_name] = float(attr_sums[attr_name] / len(instances))
-    # print()
-    # for attr_name, _mean in attr_means.items():
-    #     print('name: {}\tmean = {}'.format(attr_name, _mean))
-
+    if DEBUG:
+	print()
+	for attr_name, _mean in attr_means.items():
+	    print('name: {}\tmean = {}'.format(attr_name, _mean))
     # Find the sum of the squared differences
     for instance in instances:
         for attr_value in instance.attributes:
             if attr_names[instance.attributes.index(attr_value)] in attr_means.keys():
                 attr_sum_sq_diffs[attr_names[instance.attributes.index(attr_value)]] += \
                         (attr_value - attr_means[attr_names[instance.attributes.index(attr_value)]])**2
-    # print()
-    # for attr_name, _sum_sq_diff in attr_sum_sq_diffs.items():
-    #     print('name: {}\tsum_sq_diff = {}'.format(attr_name, _sum_sq_diff))
-
+    if DEBUG:
+	print()
+	for attr_name, _sum_sq_diff in attr_sum_sq_diffs.items():
+	    print('name: {}\tsum_sq_diff = {}'.format(attr_name, _sum_sq_diff))
     # Calculate standard deviation
     attr_std_devs = collections.OrderedDict()
     for attr_name, _sum in attr_sum_sq_diffs.items():
         attr_std_devs[attr_name] = math.sqrt( float(attr_sum_sq_diffs[attr_name] / len(instances)) )
-    # print()
-    # for attr_name, _std_dev in attr_std_devs.items():
-    #     print('name: {}\tstd_dev = {}'.format(attr_name, _std_dev))
-
+    if DEBUG:
+	print()
+	for attr_name, _std_dev in attr_std_devs.items():
+	    print('name: {}\tstd_dev = {}'.format(attr_name, _std_dev))
     # Update instances with standardized values
     std_instances = []
     for instance in instances:
@@ -97,6 +103,52 @@ class Instance(object):
         return sb
 
 
+class NodeType(Enum):
+    INPUT = 0
+    BIAS_TO_HIDDEN = 1
+    HIDDEN = 2
+    BIAS_TO_OUTPUT = 3
+    OUTPUT = 4
+
+
+class Node(object):
+    ''' A node in the net. '''
+
+    def __init__(self, node_type):
+        self.node_type = node_type
+        self.output_value = 0.0
+        self.parents = []
+        self.delta_j = 0.0
+
+    def calc_output(self):
+        ''' Calculate output for hidden and output nodes. '''
+        self.output_value = 0.0
+        if self.node_type == NodeType.HIDDEN or self.node_type == NodeType.OUTPUT:
+            for parent in parents:
+                self.output_value += parent.parent_node.get_output() * parent.weight
+            self.output_value = sigmoid(self.output_value)
+        else:
+            raise('Calculating output for non hidden/output node!')
+
+    def get_output(self):
+        if self.node_type == NodeType.BIAS_TO_HIDDEN or self.node_type == NodeType.BIAS_TO_OUTPUT:
+            return 1.0
+        else:
+            return self.output_value
+
+
+class Weight(object):
+    ''' A weight in the net. '''
+
+    def __init__(self, parent_node, weight):
+        self.parent_node = parent_node
+        self.weight = weight
+        self.delta_w = delta_w
+
+    def update_weight(self):
+        self.weight = self.weight + self.delta_w
+
+
 class Net(object):
     ''' An abstract net. '''
 
@@ -111,7 +163,8 @@ class NeuralNet(Net):
     ''' '''
 
     def __init__(self, labels, attr_names, attr_values, train_instances):
-        Net.__init__(self, labels, attr_names, attr_values, train_instances)
+        Net.__init__(self, labels, attr_names, attr_values, train_instances, num_hidden)
+	self.num_hidden = num_hidden
 
 
 class Logistic(Net):
@@ -123,16 +176,18 @@ class Logistic(Net):
 
 
 if __name__ == '__main__':
+    # 0      1   2     3    4    5      6
+    # net.py l/n train test rate epochs num_hidden
 
-    num_epochs = int(sys.argv[2])
-    learning_rate = float(sys.argv[1])
+    num_epochs = int(sys.argv[5])
+    learning_rate = float(sys.argv[4])
 
     # Load training ARFF data
-    training_set_path = str(sys.argv[3])
+    training_set_path = str(sys.argv[2])
     train_data, train_meta = arff.loadarff(training_set_path)
 
     # Load testing ARFF data
-    testing_set_path = str(sys.argv[4])
+    testing_set_path = str(sys.argv[3])
     test_data, test_meta = arff.loadarff(testing_set_path)
 
     # Create reference data structures for labels and attributes
@@ -159,17 +214,18 @@ if __name__ == '__main__':
     # print(attr_names)
     # print(attr_values)
 
-    # Create and standardize and randomize training instances
+    # Create, standardize, and randomize training instances
     train_instances = create_instances_from_arff_data(train_data, attr_names, attr_types, attr_values)
     std_train_instances = standardize_instances(train_instances, attr_names, attr_types, attr_values)
     random.shuffle(std_train_instances)
 
-    # Create and standardize testing instances
+    # Create, standardize, and randomize testing instances
     # test_instances = create_instances_from_arff_data(test_data, attr_types)
     # std_test_instances = standardize_instances(test_instances, attr_names, attr_types, attr_values)
+    # random.shuffle(std_test_instances)
 
     # Logistic Regression
-    if sys.argv[5] == 'l':
+    if sys.argv[1] == 'l':
         lr = Logistic(labels, attr_names, attr_values, std_train_instances)
 
 
@@ -187,8 +243,17 @@ if __name__ == '__main__':
 
 
 
+
+
+
+
     # Neural Net
-    elif sys.argv[5] == 'n':
-        nn = NeuralNet(labels, attr_names, attr_values, std_train_instances)
+    elif sys.argv[1] == 'n':
+        nn = NeuralNet(labels, attr_names, attr_values, std_train_instances, num_hidden)
+	num_hidden = int(sys.argv[6])
+
+
+
+
 
 
